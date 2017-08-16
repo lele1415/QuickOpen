fcee3343d29f685b8def16a8b143463d4cee6d2c
lele1415 <lele1415@163.com>
2017-08-16 19:54:04 +0800

    Version: v2.1 2017/06/22     1. ShortcutOfCodePrjOpt: 交换 代码路径 和 项目名 的位置

--- CreatShortcutOfCodePrjOpt.js
+++ CreatShortcutOfCodePrjOpt.js
@@ -8,7 +8,7 @@ function addShortcutButton(sCurrentCode, sCurrentPrj, sCurrentOpt, divId)
 	var shortcutFun = function(){applyShortcut(sCurrentCode, sCurrentPrj, sCurrentOpt)};
 	var removeFun = function(){removeShortcut(removeId, shortcutId, brId)};
 
-	var shortcutValue = " " + sCurrentCode + "\n" + sCurrentPrj + "\n" + sCurrentOpt + " "
+	var shortcutValue = " " + sCurrentOpt + "   " + sCurrentPrj + "\n" + sCurrentCode + " "
 
     parentNode_appendChild(divId, createRemoveBtn(removeId, removeFun));
     parentNode_appendChild(divId, createShortcutBtn(shortcutId, shortcutValue, shortcutFun));
